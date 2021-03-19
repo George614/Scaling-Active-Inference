a51e675bf1fb07d8ec26e5fbd5cddc36cfa69736
George <yangzhizhuo@gmail.com>
2021-03-19 13:06:45 -0400

    Remain expert_batch during the training, reduce log size & minor changes

--- train_ppl_model.py
+++ train_ppl_model.py
@@ -52,7 +52,7 @@ class ReplayBuffer(object):
         action = np.asarray(action, dtype=np.int32)
         reward = np.asarray(reward, dtype=np.float32)
         done = np.asarray(done, dtype=bool)
-        return state, action, reward, next_state, done
+        return (state, action, reward, next_state, done)
     
     def __len__(self):
         return len(self.buffer)
@@ -68,6 +68,7 @@ if __name__ == '__main__':
     buffer_size = 50000
     batch_size = 32
     grad_norm_clip = 1.0
+    log_interval = 4
     
     # use tensorboard for monitoring training if needed
     now = datetime.now()
@@ -81,6 +82,7 @@ if __name__ == '__main__':
     opt = tf.keras.optimizers.get(args.vae_optimizer)
     opt.__setattr__('learning_rate', args.vae_learning_rate)
     opt.__setattr__('epsilon', 1e-5)
+    expert_buffer = ReplayBuffer(buffer_size)
     replay_buffer = ReplayBuffer(buffer_size)
 
     # load and pre-process expert-batch data
@@ -97,13 +99,13 @@ if __name__ == '__main__':
             done = 0
             if j == np.shape(all_data)[1]-1:
                 done = 1
-                replay_buffer.push(o_t, action, reward, o_tp1, done)
+                expert_buffer.push(o_t, action, reward, o_tp1, done)
                 break
             if np.equal(all_data[i, j+1, 0], 0):
                 done = 1
-                replay_buffer.push(o_t, action, reward, o_tp1, done)
+                expert_buffer.push(o_t, action, reward, o_tp1, done)
                 break
-            replay_buffer.push(o_t, action, reward, o_tp1, done)
+            expert_buffer.push(o_t, action, reward, o_tp1, done)
 
     # load the prior preference model
     prior_model_save_path = "results/prior_model/{}/".format(args.env_name)
@@ -114,6 +116,7 @@ if __name__ == '__main__':
     losses = []
     all_rewards = []
     episode_reward = 0
+    n_episodes = 0
     crash = False
     
     env = gym.make(args.env_name)
@@ -139,81 +142,96 @@ if __name__ == '__main__':
             observation = env.reset()
             all_rewards.append(episode_reward)
             episode_reward = 0
+            n_episodes += 1
 
-        if len(replay_buffer) > batch_size:
-            batch_obv, batch_action, batch_reward, batch_next_obv, batch_done = replay_buffer.sample(batch_size)
-            batch_action = tf.one_hot(batch_action, depth=args.a_width)
-            grads_efe, grads_model, grads_l2, loss_efe, loss_model, loss_l2, R_ti, R_te, efe_t, efe_target = pplModel.train_step(batch_obv, batch_next_obv, batch_action, batch_done)
-            
-            if tf.math.is_nan(loss_efe):
-                print("loss_efe nan at frame #", frame_idx)
-                break
-            
-            # clip gradients
-            crash = False
-            grads_model_clipped = []
-            for grad in grads_model:
-                if grad is not None:
-                    grad = tf.clip_by_norm(grad, clip_norm=grad_norm_clip)
-                    if tf.math.reduce_any(tf.math.is_nan(grad)):
-                        print("grad_model nan at frame # ", frame_idx)
-                        crash = True
-                grads_model_clipped.append(grad)
-            
-            grads_efe_clipped = []
-            for grad in grads_efe:
-                if grad is not None:
-                    grad = tf.clip_by_norm(grad, clip_norm=grad_norm_clip)
-                    if tf.math.reduce_any(tf.math.is_nan(grad)):
-                        print("grad_efe nan at frame # ", frame_idx)
-                        #TODO print all weights,bias of EFE net
-                        crash = True
-                grads_efe_clipped.append(grad)
+        total_samples = len(expert_buffer) + len(replay_buffer)
+        
+        if total_samples / len(replay_buffer) < batch_size:
+            # sample from both expert buffer and replay buffer
+            n_replay_samples = np.floor(batch_size * len(replay_buffer) / total_samples)
+            n_expert_samples = batch_size - n_replay_samples
+            expert_batch = expert_buffer.sample(int(n_expert_samples))
+            replay_batch = replay_buffer.sample(int(n_replay_samples))
+            batch_data = [np.concatenate((expert_sample, replay_sample)) for expert_sample, replay_sample in zip(expert_batch, replay_batch)]
+        else:
+            # sample from expert buffer only
+            batch_data = expert_buffer.sample(batch_size)
 
-            grads_l2_clipped = []
-            for grad in grads_l2:
-                if grad is not None:
-                    grad = tf.clip_by_norm(grad, clip_norm=grad_norm_clip)
-                    if tf.math.reduce_any(tf.math.is_nan(grad)):
-                        print("grad_l2 nan at frame # ", frame_idx)
-                        crash = True
-                grads_l2_clipped.append(grad)
+        [batch_obv, batch_action, batch_reward, batch_next_obv, batch_done] = batch_data
+        
+        batch_action = tf.one_hot(batch_action, depth=args.a_width)
+        grads_efe, grads_model, grads_l2, loss_efe, loss_model, loss_l2, R_ti, R_te, efe_t, efe_target = pplModel.train_step(batch_obv, batch_next_obv, batch_action, batch_done)
+        
+        if tf.math.is_nan(loss_efe):
+            print("loss_efe nan at frame #", frame_idx)
+            break
+        
+        # clip gradients
+        crash = False
+        grads_model_clipped = []
+        for grad in grads_model:
+            if grad is not None:
+                grad = tf.clip_by_norm(grad, clip_norm=grad_norm_clip)
+                if tf.math.reduce_any(tf.math.is_nan(grad)):
+                    print("grad_model nan at frame # ", frame_idx)
+                    crash = True
+            grads_model_clipped.append(grad)
+        
+        grads_efe_clipped = []
+        for grad in grads_efe:
+            if grad is not None:
+                grad = tf.clip_by_norm(grad, clip_norm=grad_norm_clip)
+                if tf.math.reduce_any(tf.math.is_nan(grad)):
+                    print("grad_efe nan at frame # ", frame_idx)
+                    #TODO print all weights,bias of EFE net
+                    crash = True
+            grads_efe_clipped.append(grad)
 
-            if crash:
-                break
+        grads_l2_clipped = []
+        for grad in grads_l2:
+            if grad is not None:
+                grad = tf.clip_by_norm(grad, clip_norm=grad_norm_clip)
+                if tf.math.reduce_any(tf.math.is_nan(grad)):
+                    print("grad_l2 nan at frame # ", frame_idx)
+                    crash = True
+            grads_l2_clipped.append(grad)
 
-            opt.apply_gradients(
-                (grad, var) 
-                for (grad, var) in zip(grads_model_clipped, pplModel.trainable_variables) 
-                if grad is not None
-                )
-            opt.apply_gradients(
-                (grad, var) 
-                for (grad, var) in zip(grads_efe_clipped, pplModel.trainable_variables) 
-                if grad is not None
-                )
-            opt.apply_gradients(
-                (grad, var) 
-                for (grad, var) in zip(grads_l2_clipped, pplModel.trainable_variables) 
-                if grad is not None
-                )
+        if crash:
+            break
 
-            # losses.append(loss_model.numpy())
-            weights_maxes = [tf.math.reduce_max(var) for var in pplModel.trainable_variables]
-            weights_mins = [tf.math.reduce_min(var) for var in pplModel.trainable_variables]
-            weights_max = tf.math.reduce_max(weights_maxes)
-            weights_min = tf.math.reduce_min(weights_mins)
+        opt.apply_gradients(
+            (grad, var) 
+            for (grad, var) in zip(grads_model_clipped, pplModel.trainable_variables) 
+            if grad is not None
+            )
+        opt.apply_gradients(
+            (grad, var) 
+            for (grad, var) in zip(grads_efe_clipped, pplModel.trainable_variables) 
+            if grad is not None
+            )
+        opt.apply_gradients(
+            (grad, var) 
+            for (grad, var) in zip(grads_l2_clipped, pplModel.trainable_variables) 
+            if grad is not None
+            )
 
-            # grads_model_maxes = [tf.math.reduce_max(grad) for grad in grads_model if grad is not None]
-            # grads_model_mins = [tf.math.reduce_min(grad) for grad in grads_model if grad is not None]
-            # grads_max = tf.math.reduce_max(grads_model_maxes)
-            # grads_min = tf.math.reduce_min(grads_model_mins)
+        # losses.append(loss_model.numpy())
+        weights_maxes = [tf.math.reduce_max(var) for var in pplModel.trainable_variables]
+        weights_mins = [tf.math.reduce_min(var) for var in pplModel.trainable_variables]
+        weights_max = tf.math.reduce_max(weights_maxes)
+        weights_min = tf.math.reduce_min(weights_mins)
 
-            grads_efe_maxes = [tf.math.reduce_max(grad) for grad in grads_efe if grad is not None]
-            grads_efe_mins = [tf.math.reduce_min(grad) for grad in grads_efe if grad is not None]
-            grads_max = tf.math.reduce_max(grads_efe_maxes)
-            grads_min = tf.math.reduce_min(grads_efe_mins)
-            
+        # grads_model_maxes = [tf.math.reduce_max(grad) for grad in grads_model if grad is not None]
+        # grads_model_mins = [tf.math.reduce_min(grad) for grad in grads_model if grad is not None]
+        # grads_max = tf.math.reduce_max(grads_model_maxes)
+        # grads_min = tf.math.reduce_min(grads_model_mins)
+
+        grads_efe_maxes = [tf.math.reduce_max(grad) for grad in grads_efe if grad is not None]
+        grads_efe_mins = [tf.math.reduce_min(grad) for grad in grads_efe if grad is not None]
+        grads_max = tf.math.reduce_max(grads_efe_maxes)
+        grads_min = tf.math.reduce_min(grads_efe_mins)
+        
+        if frame_idx % log_interval == 0:
             tf.summary.scalar('loss_model', loss_model.numpy(), step=frame_idx)
             tf.summary.scalar('loss_efe', loss_efe.numpy(), step=frame_idx)
             tf.summary.scalar('loss_l2', loss_l2.numpy(), step=frame_idx)
@@ -230,7 +248,7 @@ if __name__ == '__main__':
             tf.summary.scalar('EFE_new_max', tf.math.reduce_max(efe_target), step=frame_idx)
             tf.summary.scalar('EFE_new_min', tf.math.reduce_min(efe_target), step=frame_idx)
             if len(all_rewards) > 0:
-                tf.summary.scalar('last_episode_rewards', all_rewards[-1], step=frame_idx)
+                tf.summary.scalar('episode_rewards', all_rewards[-1], step=n_episodes)
 
         if frame_idx % target_update_freq == 0:
             pplModel.update_target()
@@ -240,6 +258,9 @@ if __name__ == '__main__':
             print("frame {}, loss_model {}, loss_efe {}, episode_reward {}".format(frame_idx, loss_model.numpy(), loss_efe.numpy(), all_rewards[-1]))
 
     env.close()
-    # save the PPL model
-    tf.saved_model.save(pplModel, model_save_path)
-    print("> Trained the PPL model. Saved in:", model_save_path)
+    if crash:
+        print("Training crashed!!")
+    else:
+        # save the PPL model
+        tf.saved_model.save(pplModel, model_save_path)
+        print("> Trained the PPL model. Saved in:", model_save_path)
